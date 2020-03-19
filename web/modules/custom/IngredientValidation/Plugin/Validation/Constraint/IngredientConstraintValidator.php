4b987349d46c68aabc09f1516d3ce5fbc539991e
Bene Balazs <bene.balazs13@gmail.com>
2020-03-19 10:55:35 +0100

    Fix the namespace and the module name

--- web/modules/custom/IngredientValidation/Plugin/Validation/Constraint/IngredientConstraintValidator.php
+++ web/modules/custom/IngredientValidation/Plugin/Validation/Constraint/IngredientConstraintValidator.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace Drupal\IngredientValidation\src\Plugin\Validation\Constraint;
+namespace Drupal\ingredient_validation\Plugin\Validation\Constraint;
 
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
@@ -10,15 +10,13 @@ use Symfony\Component\Validator\ConstraintValidator;
 /**
  * Validates the Ingredients field on the Product CT.
  */
-class IngredientValidator extends ConstraintValidator {
+class IngredientConstraintValidator extends ConstraintValidator {
 
   /**
    * {@inheritdoc}
    */
   public function validate($value, Constraint $constraint) {
-    // $items =& $value;
-    // $entity = $this->context->getRoot()->getValue();
-    // TODO: writing the validation code
+    die('valami');
   }
 
 }
