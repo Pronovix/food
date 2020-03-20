ca8e376d81fe970da7ba6819be96a1f66f95535f
Bene Balazs <bene.balazs13@gmail.com>
2020-03-20 16:41:50 +0100

    Fix the validation, make the classes final fix the commented problems

--- web/modules/custom/ingredient_validation/src/Plugin/Validation/Constraint/IngredientConstraint.php
+++ web/modules/custom/ingredient_validation/src/Plugin/Validation/Constraint/IngredientConstraint.php
@@ -11,22 +11,16 @@ use Symfony\Component\Validator\Constraint;
  *
  * @Constraint(
  * id = "ValidIngredient",
- * label = @Translation("Prevent Product CT creation
- * if it isn't matched with the required formula", context = "Validation"),
+ * label = @Translation(" Not valid form of ingredients."),
  * type = "string"
  * )
  */
-class IngredientConstraint extends Constraint {
+final class IngredientConstraint extends Constraint {
   /**
    * Message shown when validation fails.
    *
    * @var string
    */
   public $message = 'Product creation failed: Ingredients must be in the correct form (Amount Unit Ingredient) and one in a line.';
-
-  /*
-   * {@inheritdoc}
-   */
-
 
 }
--- web/modules/custom/ingredient_validation/src/Plugin/Validation/Constraint/IngredientConstraintValidator.php
+++ web/modules/custom/ingredient_validation/src/Plugin/Validation/Constraint/IngredientConstraintValidator.php
@@ -10,22 +10,20 @@ use Symfony\Component\Validator\ConstraintValidator;
 /**
  * Validates the Ingredients field on the Product CT.
  */
-class IngredientConstraintValidator extends ConstraintValidator {
+final class IngredientConstraintValidator extends ConstraintValidator {
 
   /**
    * {@inheritdoc}
    */
-  public function validate($value, Constraint $constraint) {
-    $ingredients = $value->value;
-    $lines = explode('\n', strval($ingredients));
-    $pattern = "/^\d\s\w+\s\w+\s\w+$/";
+  public function validate($value, Constraint $constraint): void {
+    $lines = explode(PHP_EOL, strval($value->value));
+    $pattern = "/^\d+\s\w+\s\w+\s\w+\r?$/";
     foreach ($lines as $line) {
       if (!preg_match($pattern, $line)) {
         $this->context->addViolation($constraint->message);
         return;
       }
     }
-
   }
 
 }
