8c1332f3dd989b05ef08a529093bd87053bc9240
Maria Burany <mariaburany@gmail.com>
2020-04-15 19:00:46 +0200

    slpit text into array

---
+++ web/modules/custom/ingredient_term/src/Plugin/migrate/process/IngedientTermExplode.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * @file
+ * Contains \Drupal\ingredient_term\Plugin\migrate\callback\IngredientTermExplode.
+ */
+
+namespace Drupal\ingredient_term\Plugin\migrate\process;
+
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+use Drupal\ingridient_validation;
+
+/**
+ * @MigrateProcessPlugin(
+ *   id = "explode",
+ * )
+ */
+class Explode extends ProcessPluginBase {
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+  	$value = $fields['field_ingredients'];
+    $ingredients[] = explode($this->configuration[' '], $value);
+    return $ingredients[];
+  }
+}
